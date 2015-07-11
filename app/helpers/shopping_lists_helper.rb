915dd60a89ebe54655c515281748f764b0377cef
Yaniv Pinchas <yanivps@gmail.com>
2015-07-11 19:49:52 +0300

    changed measure_in_words

--- app/helpers/shopping_lists_helper.rb
+++ app/helpers/shopping_lists_helper.rb
@@ -2,6 +2,8 @@ module ShoppingListsHelper
   def measure_in_words(measure)
     text = I18n.t("measure.#{measure.unit}", default: measure.unit)
     quantity = number_with_precision(measure.quantity, strip_insignificant_zeros: true, precisiion: 2)
+    return "#{quantity}" if measure.items?
+    return "#{text}" if measure.other?
     return "#{quantity} #{text}"
   end
 
